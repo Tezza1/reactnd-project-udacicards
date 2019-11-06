5344813347ef20cb208038b107e730d579e28a60
Tezza1 <tshenker@gmail.com>
2019-11-06 18:16:52 +0900

    Feature Added dummy data
    Added dummy data for start of app

---
+++ data/data.js
@@ -0,0 +1,53 @@
+{
+  Verbs: {
+    title: 'Verbs',
+    questions: [
+      {
+        question: 'How do you say walk?',
+        answer: 'Arukimasu'
+      },
+      {
+        question: 'How do you say eat?',
+        answer: 'Tabemasu'
+      },
+      {
+        question: 'How do you say write?',
+        answer: 'Kakimasu'
+      }
+    ]
+  },
+  WeekDays: {
+    title: 'Days of the week',
+    questions: [
+      {
+        question: 'How do you say Thursday?',
+        answer: 'Mokuyobi'
+      },
+      {
+        question: 'How do you say Sunday?',
+        answer: 'Nichiyobi'
+      },
+      {
+        question: 'How do you say Monday?',
+        answer: 'Getsuyobi'
+      }
+    ]
+  },
+  Animals: {
+    title: 'Animals',
+    questions: [
+      {
+        question: 'How do you say cat?',
+        answer: 'Neko'
+      },
+      {
+        question: 'How do you say bird?',
+        answer: 'Tori'
+      },
+      {
+        question: 'How do you say dog?',
+        answer: 'Inu'
+      }
+    ]
+  }
+}
