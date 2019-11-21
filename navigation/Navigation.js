81b6ee8a3f01d8fd8e5c2d6f1bed457809c136dd
Tezza1 <tshenker@gmail.com>
2019-11-21 17:25:48 +0900

    Feature Added Bottom Nav
    Added Bottom Navigation which will take to HomeScreen

--- navigation/Navigation.js
+++ navigation/Navigation.js
@@ -1,13 +1,14 @@
+import React from 'react'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
-// import { createTabsNavigator } from 'react-navigation-tabs'
-// import { createDrawerNavigator } from 'react-navigation-drawer'
+import { createBottomTabNavigator} from 'react-navigation-tabs'
 import HomeScreen from '../screens/HomeScreen'
 import DeckScreen from '../screens/DeckScreen'
 import QuizScreen from '../screens/QuizScreen'
 import AddScreen from '../screens/AddScreen'
 import AnswerScreen from '../screens/AnswerScreen'
 import ResultScreen from '../screens/ResultScreen'
+import { Ionicons } from '@expo/vector-icons'
 
 const AppNavigator = createStackNavigator({
   Home: HomeScreen,
@@ -15,7 +16,27 @@ const AppNavigator = createStackNavigator({
   Quiz: QuizScreen,
   Add: AddScreen,
   Answer: AnswerScreen,
-  Result: ResultScreen
+  Result: ResultScreen,
 })
 
-export default createAppContainer(AppNavigator)
+const BottomTabNavigator = createBottomTabNavigator({
+  Home: {
+    screen: AppNavigator,
+    navigationOptions: {
+      title: 'Home',
+      tabBarIcon: () => (<Ionicons name={'ios-home'} size={25} horizontal={true} color={'white'}/>)
+    }
+  }
+}, {
+  tabBarOptions: {
+    activeTintColor: 'white',
+    activeBackgroundColor: '#0277bd',
+    labelStyle: {
+      fontFamily: 'openSansBold',
+      fontSize: 18,
+    },
+    tabStyle: {}
+  }
+})
+
+export default createAppContainer(BottomTabNavigator)
