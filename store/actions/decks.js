2b1f700349b006a07d6ecaf3f1afb18af864d436
Tezza1 <tshenker@gmail.com>
2019-11-23 13:15:23 +0900

    Small change

--- store/actions/decks.js
+++ store/actions/decks.js
@@ -1,12 +1,52 @@
 import { GET_DECKS, ADD_DECK, SET_DECK, ADD_QUIZ, QUESTION_NUMBER, QUIZ_PROGRESS } from './actionTypes'
 import { DECKS } from '../../data/data'
+import { STORAGE_KEY } from '../../utils/keys'
+import { AsyncStorage } from 'react-native'
 
 export const getDecks = decks => {
   return {
     type: GET_DECKS,
-    decks: DECKS
+    decks
   }
 }
+
+export const handleGetDecks = () => {
+  return async dispatch => {
+    try {
+      await AsyncStorage.getItem(STORAGE_KEY)
+        .then(decks => {
+          let data = JSON.parse(decks)
+          if (data === null){
+            dispatch(getDecks(DECKS))
+          } else {
+            dispatch(getDecks(data))
+          }
+        })
+        // for error checking
+        // await AsyncStorage.clear()
+    } catch(err) {
+      console.log(err)
+    }
+    return 'done'
+  }
+}
+
+/*export const handleGetDecks = () => {
+  return dispatch => {
+    return AsyncStorage.getItem(STORAGE_KEY)
+      .then(decks => {
+        let data = JSON.parse(decks)
+        if (data === null){
+          dispatch(getDecks(DECKS))
+        } else {
+          dispatch(getDecks(data))
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+}*/
 
 export const addDeck = title => {
   return {
